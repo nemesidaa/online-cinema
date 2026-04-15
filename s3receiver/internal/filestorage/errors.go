2803320daa4f3a685165ae5f3fae675a8c61119a
nemesidaa <salmonellarabies@gmail.com>
2026-04-15 03:10:10 +0300

    Created base handlers for async-prepared file getting

---
+++ s3receiver/internal/filestorage/errors.go
@@ -0,0 +1,7 @@
+package filestorage
+
+import "errors"
+
+var (
+	ErrBucketNotExist = errors.New("Bucket not exist")
+)
--- s3receiver/internal/filestorage/init.go
+++ s3receiver/internal/filestorage/init.go
@@ -1,20 +1,20 @@
 package filestorage
 
 import (
+	"context"
+	"io"
+	"sync"
+
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v6"
 )
+
+const _N = 6
 
 type Filestorage struct {
 	conf *FilestorageConfig
 	cli  *minio.Client
 }
-
-// ! Reference:
-// type FilestorageImplementation interface {
-// 	GetOne(uuid.UUID) (chan byte, error)
-// 	GetMany([]uuid.UUID) ([]chan byte, error)
-// }
 
 func New(conf *FilestorageConfig) (*Filestorage, error) {
 	cli, err := minio.New(conf.Address, conf.Creds.Username, conf.Creds.Password, false)
@@ -27,5 +27,114 @@ func New(conf *FilestorageConfig) (*Filestorage, error) {
 	}, nil
 }
 
-func (fs *Filestorage) GetOne(bucket uuid.UUID, id uuid.UUID) (chan byte, error)
-func (fs *Filestorage) GetMany(bucket uuid.UUID, ids []uuid.UUID) ([]chan byte, error)
+func (fs *Filestorage) GetOne(ctx context.Context, bucket uuid.UUID, id uuid.UUID) (<-chan []byte, error) {
+	ctx, cancel := context.WithTimeout(ctx, fs.conf.Timeout)
+	ok, err := fs.cli.BucketExists(bucket.String())
+	if !ok {
+		cancel()
+		return nil, ErrBucketNotExist
+	}
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+	obj, err := fs.cli.GetObject(bucket.String(), id.String(), minio.GetObjectOptions{})
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+	var respChan = make(chan []byte, _N)
+
+	go func(file *minio.Object, ch chan<- []byte) {
+		defer file.Close()
+		defer close(ch)
+		defer cancel()
+
+		buf := make([]byte, fs.conf.ChannelPayloadSize)
+
+		for {
+			n, err := file.Read(buf)
+			if n > 0 {
+				chunk := make([]byte, n)
+				copy(chunk, buf[:n])
+
+				select {
+				case ch <- chunk:
+				case <-ctx.Done():
+					return
+				}
+			}
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				break
+			}
+		}
+	}(obj, respChan)
+
+	return respChan, nil
+}
+
+func (fs *Filestorage) GetMany(ctx context.Context, bucket uuid.UUID, ids []uuid.UUID) (<-chan <-chan []byte, error) {
+	ok, err := fs.cli.BucketExists(bucket.String())
+	if !ok {
+		return nil, ErrBucketNotExist
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(ids))
+
+	respChanArr := make(chan (<-chan []byte), len(ids))
+
+	go func() {
+		wg.Wait()
+		defer close(respChanArr)
+	}()
+
+	for _, id := range ids {
+		go func(id uuid.UUID) {
+
+			ctx, cancel := context.WithTimeout(ctx, fs.conf.Timeout)
+			obj, err := fs.cli.GetObject(bucket.String(), id.String(), minio.GetObjectOptions{})
+			if err != nil {
+				cancel()
+				return
+			}
+			var respChan = make(chan []byte, _N)
+			respChanArr <- respChan
+
+			go func(file *minio.Object, ch chan<- []byte) {
+				defer file.Close()
+				defer close(ch)
+				defer cancel()
+
+				buf := make([]byte, fs.conf.ChannelPayloadSize)
+
+				for {
+					n, err := file.Read(buf)
+					if n > 0 {
+						chunk := make([]byte, n)
+						copy(chunk, buf[:n])
+
+						select {
+						case ch <- chunk:
+						case <-ctx.Done():
+							return
+						}
+					}
+					if err != nil {
+						if err == io.EOF {
+							break
+						}
+						break
+					}
+				}
+			}(obj, respChan)
+		}(id)
+	}
+	return respChanArr, nil
+}
